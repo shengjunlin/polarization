730b30e576a5dbd3e5a780f0d746b3924f95d3e5
Sheng-Jun <sebastian0117@gmail.com>
2018-06-15 11:40:27 +0800

    Add a feature to plot a histgram of pol. percentage.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ In the python shell, import polarization with the following code:
 
 Sytnax:
 
-    polseg_convert(I_map, polI_map, polPA_map, scale_10percent, sampling_interval=3, I_clip=0., polI_clip=0., seg_color='r', output_reg='output.reg')
+    polseg_convert(I_map, polI_map, polPA_map, scale_10percent, sampling_interval=3, I_clip=0., polI_clip=0., seg_color='r', output_reg='output.reg', hist_plot=True)
 
 Arguments:
 
@@ -24,6 +24,7 @@ I_clip          [float]: Exclude pixels in I_map with values <= I_clip. Default:
 polI_clip       [float]: Exclude pixels in polI_map with values <= polI_clip. Default: 0.    
 seg_color         [str]: Color of segments. Defualt: 'red'.    
 output_reg        [str]: Output region filename. Default: 'output.reg'.    
+hist_plot     [boolean]: Plot a histgram of pol. percentage. Default: True.    
 
 Generate a ds9 region file which contains polarization segments.
 
--- polarization.py
+++ polarization.py
@@ -7,7 +7,8 @@ import warnings
 
 
 def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, sampling_interval=3,
-                   I_clip=0., polI_clip=0., seg_color='r', output_reg='output.reg'):
+                   I_clip=0., polI_clip=0., seg_color='r', output_reg='output.reg',
+                   hist_plot=True):
     """
     polseg_convert(I_map, polI_map, polPA_map,
         scale_10percent, sampling_interval, seg_color, output_reg)
@@ -22,6 +23,7 @@ def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, sampling_interva
     polI_clip       [float]: Exclude pixels in polI_map with values <= polI_clip. Default: 0.
     seg_color         [str]: Color of segments. Defualt: 'red'.
     output_reg        [str]: Output region filename. Default: 'output.reg'.
+    hist_plot     [boolean]: Plot a histgram of pol. percentage. Default: True.
     """
     # Open each fits flie
     I_hdulist = pyfits.open(I_map)
@@ -50,6 +52,10 @@ def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, sampling_interva
         'select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 '
         'source=1\nfk5\n'.format(seg_color))
 
+    if hist_plot:
+        from matplotlib.pyplot import hist, xlabel, ylabel, show
+    polper_ls = []
+
     # Get the coordinates (of world coor. system) of polarized intensity map
     wcs = WCS(polI_hd)
     for j in xrange(Ny):
@@ -64,12 +70,20 @@ def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, sampling_interva
                 ra = wcs.all_pix2world(nx[j, i], ny[j, i], 0, 0, 0)[0]
                 dec = wcs.all_pix2world(nx[j, i], ny[j, i], 0, 0, 0)[1]
                 # Calculate an half of length of segments:
+                polper = polI/I
+                polper_ls.append(polper)
                 # 0.5 * (polI/I) * sin[or cos](PA) * (10*scale_10percent[arcs])/3600 in deg
                 dx_half = 0.5 / 360 * scale_10percent * \
-                    polI / I * np.sin(polPA) / np.cos(dec / 180. * np.pi) # dRA is corrected by cos(Dec)
+                    polper * np.sin(polPA) / np.cos(dec / 180. * np.pi) # dRA is corrected by cos(Dec)
                 dy_half = 0.5 / 360 * scale_10percent * \
-                    polI / I * np.cos(polPA)
+                    polper * np.cos(polPA)
                 reg_file.write("line({0},{1},{2},{3}) # line=0 0\n".format(
                     ra + dx_half, dec + dy_half, ra - dx_half, dec - dy_half))
 
     reg_file.close()
+
+    if hist_plot:
+        hist(polper_ls)
+        xlabel('polarized percentage')
+        ylabel('N')
+        show()
