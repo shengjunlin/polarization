1b3c3b071dcd40973b0f0abd8da6b349cd5461dc
shengjunlin <sj.lin@gapp.nthu.edu.tw>
2021-03-23 05:25:36 +0800

    Python3 compatible. Add new parameters.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # polarization
-Python functions related to ploarization.
+A Python function related to polarization.
 
 In the python shell, import polarization with the following code:  
 
@@ -9,25 +9,34 @@ In the python shell, import polarization with the following code:
 
 **polseg_convert()**
 
-Sytnax:
+Syntax:
 
-    polseg_convert(I_map, polI_map, polPA_map, scale_10percent, sampling_interval=3, I_clip=0., polI_clip=0., seg_color='r', output_reg='output.reg', hist_plot=True)
+    polseg_convert(I_map, polI_map, polPA_map='',
+                   scale_10percent=10., sampling_interval=3,
+                   uniform_PA=np.nan, uniform_scale=np.nan, PA_offset=0.,
+                   i_hdu=0, i_chan=0, I_clip=0., polI_clip=0.,
+                   seg_color='red', output_reg='output.reg', hist_plot=True)
 
 Arguments:
 
-I_map             [str]: The fits filename of Stokes I.    
-polI_map          [str]: The fits filename of Polarized intensity.    
-polPA_map         [str]: The fits filename of PA[deg] of polarization segments.    
-scale_10percent [float]: Length[arcsec] of 10% polarization segments.    
-sampling_interval [int]: (1/sampling rate of segments)[pixel]. Default: 3.    
-I_clip          [float]: Exclude pixels in I_map with values <= I_clip. Default: 0.    
-polI_clip       [float]: Exclude pixels in polI_map with values <= polI_clip. Default: 0.    
-seg_color         [str]: Color of segments. Defualt: 'red'.    
-output_reg        [str]: Output region filename. Default: 'output.reg'.    
-hist_plot     [boolean]: Plot a histgram of pol. percentage. Default: True.    
+I_map             [str]: The fits filename of Stokes I map.    
+polI_map          [str]: The fits filename of the polarized intensity map.    
+polPA_map         [str]: The fits filename of PA [deg] of polarization segments. (Default value = '')    
+scale_10percent [float]: The segment length [arcsec] of polarization fraction = 10%. (Default value = 10)    
+sampling_interval [int]: (1/sampling rate of segments)[pixel]. (Default value = 3)    
+uniform_PA      [float]: Assgin a single PA [deg] to create the segments, which will overwrite "polPA_map" if it isn't a np.nan. (Default value = np.nan)    
+uniform_scale   [float]: Create the segments with uniform lengths [arcsec], which will overwrite "scale_10percent" if it isn't a np.nan. (Default value = np.nan)    
+PA_offset       [float]: Additional PA offset [deg]. That is, the resulting PA = PA read from polPA_map + PA_offset, or PA = uniform_PA + PA_offset. (Default value = 0.)    
+i_hdu             [int]: The number in the hdu list. (Default value = 0)    
+i_chan            [int]: Channel number. (Default value = 0)    
+I_clip          [float]: Exclude pixels in I_map with abs(I) <= I_clip. (Default value = 0)    
+polI_clip       [float]: Exclude pixels in polI_map with polI <= polI_clip. (Default value = 0)    
+seg_color         [str]: Color of segments. (Defualt value = 'red')    
+output_reg        [str]: Output region filename. (Default value = 'output.reg')    
+hist_plot        [bool]: Plot a histgram of polarization fraction. (Default value = True)    
 
-Generate a ds9 region file which contains polarization segments.
+Use the small-angle approximation to calculate the coordinates of the endpoints of segments, and generate a ds9 region file storing the polarization segments.
 
-Assume the fits files have 4 axes. (e.g. CASA simulation outputs)
+The input fits files can have either 4 (e.g., CASA simulation outputs), 3 (freq/vel, Dec, RA), or 2 (Dec, RA) axes.
 
 Please checkout help(polseg_convert)
--- polarization.py
+++ polarization.py
@@ -3,30 +3,50 @@
 import numpy as np
 import astropy.io.fits as pyfits
 from astropy.wcs import WCS
-import warnings
+import sys, warnings
 
+if (sys.version_info.major >= 3):
+    # Python3
+    xrange = range
 
-def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, i_hdu=0, i_chan=0, sampling_interval=3,
-                   I_clip=0., polI_clip=0., seg_color='red', uniformPA=np.nan, output_reg='output.reg',
-                   hist_plot=True):
+def polseg_convert(I_map, polI_map, polPA_map='',
+                   scale_10percent=10., sampling_interval=3,
+                   uniform_PA=np.nan, uniform_scale=np.nan, PA_offset=0.,
+                   i_hdu=0, i_chan=0, I_clip=0., polI_clip=0.,
+                   seg_color='red', output_reg='output.reg', hist_plot=True):
     """
-    polseg_convert(I_map, polI_map, polPA_map,
-        scale_10percent, sampling_interval, seg_color, output_reg)
-    Generate a ds9 region file which contains polarization segments.
-    Assume the fits files have 4 axes. (e.g. CASA simulation outputs)
-    I_map             [str]: The fits filename of Stokes I.
-    polI_map          [str]: The fits filename of Polarized intensity.
-    polPA_map         [str]: The fits filename of PA[deg] of polarization segments.
-    scale_10percent [float]: Length[arcsec] of 10% polarization segments.
-    i_hdu             [int]: The number in the hdu list. Default: 0.
-    i_chan            [int]: Channel number. Default: 0.
-    sampling_interval [int]: (1/sampling rate of segments)[pixel]. Default: 3.
-    I_clip          [float]: Exclude pixels in I_map with values <= I_clip. Default: 0.
-    polI_clip       [float]: Exclude pixels in polI_map with values <= polI_clip. Default: 0.
-    seg_color         [str]: Color of segments. Defualt: 'red'.
-    uniformPA
-    output_reg        [str]: Output region filename. Default: 'output.reg'.
-    hist_plot     [boolean]: Plot a histgram of pol. percentage. Default: True.
+    polseg_convert(I_map, polI_map, polPA_map='',
+                   scale_10percent=10., sampling_interval=3,
+                   uniform_PA=np.nan, uniform_scale=np.nan, PA_offset=0.,
+                   i_hdu=0, i_chan=0, I_clip=0., polI_clip=0.,
+                   seg_color='red', output_reg='output.reg', hist_plot=True)
+
+    Use the small-angle approximation to calculate the coordinates of the endpoints of segments,
+    and generate a ds9 region file storing the polarization segments.
+    The input fits files can have either 4 (e.g., CASA simulation outputs),
+                                      or 3 (freq/vel, Dec, RA),
+                                      or 2 (Dec, RA) axes.
+
+    Args:
+      I_map             [str]: The fits filename of Stokes I map.
+      polI_map          [str]: The fits filename of the polarized intensity map.
+      polPA_map         [str]: The fits filename of PA [deg] of polarization segments. (Default value = '')
+      scale_10percent [float]: The segment length [arcsec] of polarization fraction = 10%. (Default value = 10)
+      sampling_interval [int]: (1/sampling rate of segments)[pixel]. (Default value = 3)
+      uniform_PA      [float]: Assgin a single PA [deg] to create the segments,
+                               which will overwrite "polPA_map" if it isn't a np.nan. (Default value = np.nan)
+      uniform_scale   [float]: Create the segments with uniform lengths [arcsec],
+                               which will overwrite "scale_10percent" if it isn't a np.nan. (Default value = np.nan)
+      PA_offset       [float]: Additional PA offset [deg]. (Default value = 0.)
+                               That is, the resulting PA = PA read from polPA_map + PA_offset,
+                                                   or PA = uniform_PA + PA_offset.
+      i_hdu             [int]: The number in the hdu list. (Default value = 0)
+      i_chan            [int]: Channel number. (Default value = 0)
+      I_clip          [float]: Exclude pixels in I_map with abs(I) <= I_clip. (Default value = 0)
+      polI_clip       [float]: Exclude pixels in polI_map with polI <= polI_clip. (Default value = 0)
+      seg_color         [str]: Color of segments. (Defualt value = 'red')
+      output_reg        [str]: Output region filename. (Default value = 'output.reg')
+      hist_plot        [bool]: Plot a histgram of polarization fraction. (Default value = True)
     """
     # Open each fits flie
     I_hdulist = pyfits.open(I_map)
@@ -70,9 +90,12 @@ def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, i_hdu=0, i_chan=
         'select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 '
         'source=1\nfk5\n'.format(seg_color))
 
-    if hist_plot:
-        from matplotlib.pyplot import hist, xlabel, ylabel, show
     polper_ls = []
+    if np.isnan(uniform_scale):
+        # 100% length = (10*scale_10percent[arcsec])/3600 in degree
+        segscale_deg = scale_10percent / 360.
+    else:
+        segscale_deg = uniform_scale / 3600.
 
     # Get the coordinates (of world coor. system) of polarized intensity map
     wcs = WCS(polI_hd)
@@ -81,10 +104,10 @@ def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, i_hdu=0, i_chan=
             # Get the I, polI and polPA for each pixel
             I = I_data[j, i]
             polI = polI_data[j, i]
-            if np.isnan(uniformPA):
-                polPA_rad = polPA_data[j, i] / 180. * np.pi
+            if np.isnan(uniform_PA):
+                polPA_rad = (polPA_data[j, i] + PA_offset) / 180. * np.pi
             else:
-                polPA_rad = uniformPA / 180. * np.pi
+                polPA_rad = (uniform_PA + PA_offset) / 180. * np.pi
             if abs(I) > I_clip and polI > polI_clip and not np.isnan(polPA_rad) \
                     and i % sampling_interval == 0 and j % sampling_interval == 0:
                 # Get the RA and Dec in deg for each pixel
@@ -92,27 +115,24 @@ def polseg_convert(I_map, polI_map, polPA_map, scale_10percent, i_hdu=0, i_chan=
                     # 4-dim
                     ra_deg, dec_deg, _, _ = wcs.all_pix2world(nx[j, i], ny[j, i], 0, 0, 0)
                 except:
-                    try:
+                    try:  # 3-dim
                         ra_deg, dec_deg, _ = wcs.all_pix2world(nx[j, i], ny[j, i], 0, 0)
-                    except:
+                    except:  # 2-dim
                         ra_deg, dec_deg = wcs.all_pix2world(nx[j, i], ny[j, i], 0)
-                # ra_deg = wcs.all_pix2world(nx[j, i], ny[j, i], 0, 0)[0]
-                # dec_deg = wcs.all_pix2world(nx[j, i], ny[j, i], 0, 0)[1]
                 # Calculate an half of length of segments:
                 polper = polI/I
                 polper_ls.append(polper)
                 # 0.5 * (polI/I) * sin[or cos](PA) * (10*scale_10percent[arcs])/3600 is in deg
                 # dRA is corrected by cos(Dec)
-                dx_half_deg = 0.5 * polper * np.sin(polPA_rad) / np.cos(dec_deg / 180. * np.pi) \
-                    * scale_10percent / 360.
-                dy_half_deg = 0.5 * polper * np.cos(polPA_rad) \
-                    * scale_10percent / 360.
+                dx_half_deg = 0.5 * segscale_deg * polper * np.sin(polPA_rad) / np.cos(dec_deg / 180. * np.pi)
+                dy_half_deg = 0.5 * segscale_deg * polper * np.cos(polPA_rad)
                 reg_file.write("line({0},{1},{2},{3}) # line=0 0\n".format(
                     ra_deg + dx_half_deg, dec_deg + dy_half_deg, ra_deg - dx_half_deg, dec_deg - dy_half_deg))
 
     reg_file.close()
 
     if hist_plot:
+        from matplotlib.pyplot import hist, xlabel, ylabel, show
         hist(polper_ls)
         xlabel('polarized percentage')
         ylabel('N')
